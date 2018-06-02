a8469ea4d4c98bc0c6196c73779a6188a259317e
mark-oblack <moblack42@gmail.com>
2018-06-02 16:29:09 -0400

    still working on unmatched() functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 
+const card = document.getElementsByClassName("card");
+const cards = [...card];
+const deck = document.getElementById("card-deck");
+let moves = document.querySelector(".moves");
+let count = 0;
+let openCards = [];
+let isClicked;
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -14,31 +22,11 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-const card = document.getElementsByClassName("card");
-const cards = [...card];
-const deck = document.getElementById("card-deck");
-let moves = document.querySelector(".moves");
-let count = 0;
-let openCards = [];
-let isClicked;
-
 function startGame() {
     isClicked = false;
     clickEvents();
     count = 0;
-    moves.innerHTML = 0;
+    moves.innerHTML = count;
     $("#card-deck").children().css("background", "#485460");
     $(".card").children().css("display", "none");
 	let shuffledCards = shuffle(cards);
@@ -71,14 +59,15 @@ function starRating() {
 function matched() {
     for(var i = 0; i < openCards.length; i++) {
         openCards[i].style.backgroundColor = "#0be881";
-        isClicked = true;
-        clickEvents();
+        openCards[i].removeEventListener("click", displayCard);
+        openCards[i].removeEventListener("click", cardOpen);
     }
 }
 
 function unmatched() {
     for(var i = 0; i < openCards.length; i++) {
-        openCards[i].style.backgroundColor = "#ff3838";
+        openCards[i].style.backgroundColor = "#485460";
+        // openCards[i].classList.add("unmatched");
     }
 }
 
@@ -114,6 +103,7 @@ function clickEvents() {
 }
 
  window.onload = startGame();
+ window.onload = clickEvents();
 
 
 
